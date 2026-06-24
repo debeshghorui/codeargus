6b5c62f72a12085ffc0d16d313f777518f1cd57b
Debesh Ghorui <165417580+debeshghorui@users.noreply.github.com>
2026-06-24 14:04:31 +0530

    Update some configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    allowedDevOrigins: ["darkness-yen-expletive.ngrok-free.dev"],
 };
 
 export default nextConfig;
